0a0a1281d7d38956adfb8e3e58e2c95858cdc756
Mato <mato.kubini@gmail.com>
2024-06-22 09:18:22 +0200

    pridane nove foldery

--- src/Grafy.tsx
+++ src/Grafy.tsx
@@ -21,37 +21,37 @@ function DashBoard() {
         </div>
         <div className="p-5 border-2 rounded-lg">
           <div className="flex flex-row justify-between items-center mb-2">
-            <h2 className="text-xl font-medium ">Hodnota Tenderu</h2>
+            <h2 className="text-xl font-medium ">Čas na dokument</h2>
             <i className="bx bx-dots-horizontal-rounded"></i>
           </div>
           <div className="w-full border-2 mb-5"></div>
-          <p className="text-sm font-medium text-gray-500">Avg. hodnota tenderu  za celý čas</p>
+          <p className="text-sm font-medium text-gray-500">Čas strávený na vyrábaní dokumentu</p>
           <div className="flex justify-between items-center mt-4">
-            <p className="text-3xl font-medium">35600€</p>
+            <p className="text-3xl font-medium">03'60''</p>
             <img src={Img} width={85} alt="" />
           </div>
         </div>
         <div className="p-5 border-2 rounded-lg">
           <div className="flex flex-row justify-between items-center mb-2">
-            <h2 className="text-xl font-medium ">Hodnota Tenderu</h2>
+            <h2 className="text-xl font-medium ">Typ používateľa</h2>
             <i className="bx bx-dots-horizontal-rounded"></i>
           </div>
           <div className="w-full border-2 mb-5"></div>
-          <p className="text-sm font-medium text-gray-500">Avg. hodnota tenderu  za celý čas</p>
+          <p className="text-sm font-medium text-gray-500">Najbežnejší používatelia</p>
           <div className="flex justify-between items-center mt-4">
-            <p className="text-3xl font-medium">35600€</p>
+            <p className="text-3xl font-medium">Firmy</p>
             <img src={Img} width={85} alt="" />
           </div>
         </div>
         <div className="p-5 border-2 rounded-lg">
           <div className="flex flex-row justify-between items-center mb-2">
-            <h2 className="text-xl font-medium ">Hodnota Tenderu</h2>
+            <h2 className="text-xl font-medium ">Typ podľa šablóny</h2>
             <i className="bx bx-dots-horizontal-rounded"></i>
           </div>
           <div className="w-full border-2 mb-5"></div>
-          <p className="text-sm font-medium text-gray-500">Avg. hodnota tenderu  za celý čas</p>
+          <p className="text-sm font-medium text-gray-500">Najbežnejšie firmy</p>
           <div className="flex justify-between items-center mt-4">
-            <p className="text-3xl font-medium">35600€</p>
+            <p className="text-3xl font-medium">Firmy</p>
             <img src={Img} width={85} alt="" />
           </div>
         </div>
